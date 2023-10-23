2c5259f6b74d776b02b566e07397052cf1326507
milton munoz <livemilton@hotmail.com>
2023-10-22 20:02:37 -0500

    All resources and challenge 1 your initials

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("M   M  FFFFF");
+        System.out.println("MM MM  F    ");
+        System.out.println("M M M  F    ");
+        System.out.println("M   M  FFF  ");
+        System.out.println("M   M  F    ");
+        System.out.println("M   M  F    ");
+        System.out.println("M   M  F    ");
+    }
+}
