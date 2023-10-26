114c22df393e3d8250f8696a3d3a5128602efc07
milton munoz <livemilton@hotmail.com>
2023-10-25 22:06:39 -0500

    long, operators, cast, workbook 2.3

---
+++ Module 1 - Introduction to Java/2. Variables/AVariablesUdemy/Decimals.java
@@ -0,0 +1,13 @@
+public class Decimals {
+
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up");
+
+        double wallet = 20; // always use double for math calculation. This eill be stored as a double:
+                            // 20.00000
+        double people = 3;
+        System.out.println(wallet / people);
+    }
+
+}
---
+++ Module 1 - Introduction to Java/2. Variables/AVariablesUdemy/WholeNumbers.java
@@ -0,0 +1,10 @@
+public class WholeNumbers {
+
+    public static void main(String[] args) {
+        long globalPopulation = 8000_000_000L;
+        System.out.println("The global population is " + globalPopulation);
+
+        long dailyGoogleSearches = 8500_000_000L;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+    }
+}
