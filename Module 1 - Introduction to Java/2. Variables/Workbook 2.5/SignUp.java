b5992b1db57450eb73e0747056163d4af2d8b554
MILTON FABIAN MUÑOZ RIAÑO <MIMUNOZ@davivienda.com>
2023-10-28 08:11:05 -0500

    nextLine trap, signUp and challenge with chatbot activity

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -3,35 +3,49 @@ import java.util.Scanner;
 public class SignUp {
 
     public static void main(String[] args) {
-        
+
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.5 to access the link).
 
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Welcome to JavaGram! Let's sign you up.");
-        
+
         System.out.println("What is your first name?");
-        
+        String name = scan.next();
+        scan.nextLine();// trap empty space
+
         System.out.println("What is your last name?");
+        String lastname = scan.next();
+        scan.nextLine();// trap empty space
 
         System.out.println("How old are you?");
+        int age = scan.nextInt();
+        scan.nextLine(); // trap empty space
 
-        System.out.println("Make a username");  
+        System.out.println("Make a username");
+        String username = scan.next();
+        scan.nextLine(); // trap empty space
         // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
-        
+
         System.out.println("What city do you live in?");
+        String city = scan.next();
+        scan.nextLine();// trap empty space
 
         System.out.println("What country is that?");
-        
+        String country = scan.next();
+        scan.nextLine();// trap empty space
+
         System.out.println("Thank you for joining JavaGram!");
         System.out.println("\nHere is the information you entered:");
-        System.out.println("\tFirst Name: <firstName>");
-        System.out.println("\tLast Name: <lastName>");
-        System.out.println("\tAge: <age>");
-        System.out.println("\tUsername: <userName>");
-        System.out.println("\tCity: <city>");
-        System.out.println("\tCountry: <country>");
+        System.out.println("\tFirst Name: " + name);
+        System.out.println("\tLast Name: " + lastname);
+        System.out.println("\tAge: " + age);
+        System.out.println("\tUsername: " + username);
+        System.out.println("\tCity: " + city);
+        System.out.println("\tCountry: " + country);
 
-        //close scanner. It's good practice :D ! 
+        scan.close();
+
+        // close scanner. It's good practice :D !
     }
 }
